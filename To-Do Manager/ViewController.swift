6a1e5180b215c9e3776a780ab192ec48dfec9776
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2023-07-02 13:14:42 +0200

    Initial Commit

---
+++ To-Do Manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To-Do Manager
+//
+//  Created by Maryna Bolotska on 02/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
